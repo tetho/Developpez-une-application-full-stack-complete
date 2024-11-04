8d684e9bbe4235b7090ced79fe14de072beb356a
Jerome <jerome.s42@hotmail.fr>
2024-11-04 15:00:59 +0100

    client side : empty components, interfaces, services created

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,12 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TopicComponent } from './topic/topic.component';
+import { PostListComponent } from './features/posts/components/post-list/post-list.component';
+import { PostDetailComponent } from './features/posts/components/post-detail/post-detail.component';
+import { PostFormComponent } from './features/posts/components/post-form/post-form.component';
+import { CommentFormComponent } from './features/comments/components/comment-form/comment-form.component';
+import { CommentListComponent } from './features/comments/components/comment-list/comment-list.component';
+import { CommentDetailComponent } from './features/comments/components/comment-detail/comment-detail.component';
+import { TopicListComponent } from './features/topics/components/topic-list/topic-list.component';
+import { TopicDetailComponent } from './features/topics/components/topic-detail/topic-detail.component';
+import { TopicFormComponent } from './features/topics/components/topic-form/topic-form.component';
+import { LoginComponent } from './features/auth/components/login/login.component';
+import { RegisterComponent } from './features/auth/components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopicComponent
+    PostListComponent,
+    PostDetailComponent,
+    PostFormComponent,
+    CommentFormComponent,
+    CommentListComponent,
+    CommentDetailComponent,
+    TopicListComponent,
+    TopicDetailComponent,
+    TopicFormComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TopicComponent } from './topic.component';
+import { LoginComponent } from './login.component';
 
-describe('TopicComponent', () => {
-  let component: TopicComponent;
-  let fixture: ComponentFixture<TopicComponent>;
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TopicComponent ]
+      declarations: [ LoginComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(TopicComponent);
+    fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
