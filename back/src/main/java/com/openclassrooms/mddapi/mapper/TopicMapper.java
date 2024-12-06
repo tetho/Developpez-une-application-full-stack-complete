2f244cb81ad28dbe4e41945ad8b26b30f2303848
Jerome <jerome.s42@hotmail.fr>
2024-12-06 13:21:34 +0100

    server side : topic mapper updated

--- back/src/main/java/com/openclassrooms/mddapi/mapper/TopicMapper.java
+++ back/src/main/java/com/openclassrooms/mddapi/mapper/TopicMapper.java
@@ -1,6 +1,7 @@
 package com.openclassrooms.mddapi.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import com.openclassrooms.mddapi.dto.TopicDTO;
@@ -11,7 +12,9 @@ public interface TopicMapper {
 
 	TopicMapper INSTANCE = Mappers.getMapper(TopicMapper.class);
 
+    @Mapping(source = "description", target = "description")
 	TopicDTO toDTO(Topic topic);
     
+    @Mapping(source = "description", target = "description")
 	Topic toEntity(TopicDTO topicDTO);
 }
